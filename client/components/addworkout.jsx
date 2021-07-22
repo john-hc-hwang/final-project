9d0ae059c642df136bda0780fa797d28d387b0e0
John Hwang <johnhchwang@gmail.com>
2021-07-21 23:38:50 -0700

    current work

--- client/components/addworkout.jsx
+++ client/components/addworkout.jsx
@@ -3,21 +3,96 @@ export default class AddWorkout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      AddModalActive: false
+      addModalActive: false,
+      exercise: '',
+      weight: '',
+      sets: '',
+      reps: '',
+      rest: ''
     };
 
     this.toggleState = this.toggleState.bind(this);
+    this.setExercise = this.setExercise.bind(this);
+    this.setWeight = this.setWeight.bind(this);
+    this.setSets = this.setSets.bind(this);
+    this.setReps = this.setReps.bind(this);
+    this.setRest = this.setRest.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
 
   toggleState() {
-    this.setState({ AddModalActive: !this.state.AddModalActive });
+    this.setState({ addModalActive: !this.state.addModalActive });
+  }
+
+  setExercise(event) {
+    this.setState({ exercise: event.target.value });
+  }
+
+  setWeight(event) {
+    this.setState({ weight: event.target.value });
+  }
+
+  setSets(event) {
+    this.setState({ sets: event.target.value });
+  }
+
+  setReps(event) {
+    this.setState({ reps: event.target.value });
+  }
+
+  setRest(event) {
+    this.setState({ rest: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      exercise: '',
+      weight: '',
+      sets: '',
+      reps: '',
+      rest: ''
+    });
+
+    event.preventDefault();
+  }
+
+  showModal() {
+    if (this.state.addModalActive) {
+      return (
+        <form onSubmit={ this.handleSubmit } className="overlay">
+          <div className="modal-container">
+            <div className="modal-options">
+              <button className="modal-button" onClick={ this.toggleState }>Cancel</button>
+              <p className="modal-title">New Workout</p>
+              <button className="modal-button">Add</button>
+            </div>
+            <p className="modal-date">07 21 2021</p>
+            <label htmlFor="exerciseInput">Exercise Name</label>
+            <input required id="exerciseInput" type="text" value={ this.state.exercise } onChange={ this.setExercise }/>
+            <label htmlFor="weightInput">Weight (lbs)</label>
+            <input required id="weightInput" type="number" min="0" value={ this.state.weight } onChange={ this.setWeight }/>
+            <label htmlFor="setInput">Sets</label>
+            <input required id="setInput" type="number" min="1" value={ this.state.sets } onChange={ this.setSets }/>
+            <label htmlFor="repInput">Reps</label>
+            <input required id="repInput" type="number" min="1" value={ this.state.reps } onChange={ this.setReps }/>
+            <label htmlFor="restInput">Rest Time (sec)</label>
+            <input required id="restInput" type="number" min="0" value={ this.state.rest } onChange={ this.setRest }/>
+          </div>
+        </form>
+      );
+    }
   }
 
   render() {
-    console.log(this.state.AddModalActive);
     return (
       <>
-        <button onClick={ this.toggleState } className="main-button">Add Workout</button>
+        <button
+        onClick={ this.toggleState }
+        className="main-button">
+          Add Workout
+        </button>
+        { this.showModal() }
       </>
     );
   }
