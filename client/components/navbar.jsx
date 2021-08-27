c7d3d5c455505581501103740438cf42f342c813
John Hwang <johnhchwang@gmail.com>
2021-08-27 16:14:49 -0700

    comments added

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -5,9 +5,11 @@ export default class NavBar extends React.Component {
   //   super(props);
   // }
 
+  // add below tab when there are multiple pages to show
   // tab
   // <i className="fas fa-align-justify"></i> //
 
+  // add below user when user registration and sign in is implemented
   // user
   // <i className="far fa-user"></i>
 
